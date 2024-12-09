d67e13508ffd347d315356e761532505103ef231
Nazmi Yavuz <nazmiyavuz@gmail.com>
2024-12-09 19:47:50 +0300

    Change the package.swift

--- Package.swift
+++ Package.swift
@@ -9,14 +9,11 @@ let package = Package(
       .macOS(.v12), .iOS(.v12)
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SwiftBeacon",
-            targets: ["SwiftBeacon"]),
+            targets: ["SwiftBeacon"])
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "SwiftBeacon",
             path: "SwiftBeacon/Classes")
